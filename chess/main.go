248834ba2583d2baef2fd15c879fa75b8277ddb8
bulatenkom <bulatenkom@gmail.com>
2025-05-18 18:38:31 +0700

    update chess rendering

--- chess/main.go
+++ chess/main.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 type app struct {
@@ -22,6 +23,7 @@ func (app *app) gameloop() error {
 	helpRegex := regexp.MustCompile(`^(?:\?|\/\?|help|h)$`)
 	avMovesRegex := regexp.MustCompile(`^[a-h][1-8]$`)
 	moveRegex := regexp.MustCompile(`^([a-h][1-8]){2}$`)
+	histRegex := regexp.MustCompile(`^(hist|history)$`)
 
 	for {
 		// paint UI
@@ -43,13 +45,21 @@ func (app *app) gameloop() error {
 		case helpRegex.MatchString(input):
 			// toggle help
 			app.ui.banner = "toggle help"
+			app.ui.sideview = helpTemplate()
+		case histRegex.MatchString(input):
+			// toggle help
+			app.ui.banner = "toggle history"
+			app.ui.sideview = app.ui.history
 		case avMovesRegex.MatchString(input):
 			// togle movements for piece
 			app.ui.banner = "toggle movements"
+			app.ui.sideview = "(moves goes here...)"
 		case moveRegex.MatchString(input):
 			// handle move
 			app.ui.banner = "handle move"
 			app.moveCount++
+			app.ui.history += fmt.Sprintf(" %v. %v \n", app.moveCount, input)
+			app.ui.sideview = app.ui.history
 		default:
 			app.ui.err = fmt.Errorf("command '%s' is unknown. Type 'h' to get full list of commands", input)
 			continue
@@ -59,18 +69,37 @@ func (app *app) gameloop() error {
 	}
 }
 
+func helpTemplate() string {
+	return `HELP
+  help, h, ?, /? - toggles help window
+  [a-h][1-8] - prints possible moves for selected piece (e.g. 'e2' prints moves for Pawn)
+  [a-h][1-8][a-h][1-8] - move piece (e.g. 'e2e4' moves piece from e2 to e4)
+`
+}
+
 // paintUI()
 func (app *app) paintUI() error {
 	if _, err := clearScreen(); err != nil {
 		return err
 	}
-	mainview := renderView(
-		ansiYellow(app.ui.banner),
-		renderBoard(app.board),
-		"move count: "+strconv.Itoa(app.moveCount),
+	mainview := Layout(
+		[]Box{
+			NewBox(ansiYellow(app.ui.banner)),
+			NewBox(renderBoard(app.board)),
+			NewBox("move count: " + strconv.Itoa(app.moveCount)),
+		},
+		Vertical,
 	)
-	layout := mainview.content
-	if _, err := fmt.Println(layout); err != nil {
+	sideview := Layout(
+		[]Box{
+			NewBox(app.ui.sideview),
+			// NewBox(ansiRed(app.ui.sideview)),
+		},
+		Vertical,
+	)
+	layout := Layout([]Box{mainview, sideview}, Horizontal)
+	// layout := Layout([]Box{sideview}, Horizontal)
+	if _, err := fmt.Println(layout.String()); err != nil {
 		return err
 	}
 	if app.ui.err != nil {
@@ -81,17 +110,6 @@ func (app *app) paintUI() error {
 	return nil
 }
 
-func renderView(components ...string) Box {
-	mainview := ""
-	for _, v := range components {
-		mainview += v
-		if v[len(v)-1] != '\n' {
-			mainview += "\n"
-		}
-	}
-	return NewBox(mainview)
-}
-
 func clearScreen() (int, error) {
 	return fmt.Print("\x1b[H\x1b[2J")
 }
@@ -100,7 +118,11 @@ func clearScreen() (int, error) {
 type Box struct {
 	// width, height
 	w, h    int
-	content string
+	content []string
+}
+
+func (b *Box) String() string {
+	return strings.Join(b.content, "")
 }
 
 func NewBox(s string) Box {
@@ -110,18 +132,20 @@ func NewBox(s string) Box {
 	}
 	width := 0
 	for _, line := range lines {
-		width = max(width, len(line))
+		width = max(width, CountRunesIgnoringAnsiEscapes(line))
 	}
-	content := ""
+	content := []string{}
 	for _, line := range lines {
-		buf := make([]rune, width+1)
-		copy(buf, []rune(line))
-		for i := len(line); i < width; i++ {
-			buf[i] = '-'
-		}
-		buf[width] = '\n'
+		// buf := make([]rune, width+1) // reserve place for '\n'
+		// copy(buf, []rune(line))
+		// for i := utf8.RuneCountInString(line); i < width; i++ {
+		// 	buf[i] = '\x00'
+		// }
+		// buf[width] = '\n'
+		// content = append(content, string(buf))
 
-		content += string(buf)
+		fline := line + strings.Repeat(" ", width-CountRunesIgnoringAnsiEscapes(line)) + "\n"
+		content = append(content, fline)
 	}
 
 	return Box{
@@ -129,6 +153,59 @@ func NewBox(s string) Box {
 		h:       len(lines),
 		content: content,
 	}
+}
+
+var escapeSeqMatcher *regexp.Regexp = regexp.MustCompile("(\033\\[0m|\033\\[31m|\033\\[33m|\033\\[1m|\033\\[38;2;0;0;0m|\033\\[48;2;209;139;71m|\033\\[48;2;255;206;158m)")
+
+func CountRunesIgnoringAnsiEscapes(s string) int {
+	res := escapeSeqMatcher.ReplaceAllLiteralString(s, "")
+	return utf8.RuneCountInString(res)
+}
+
+type Orientation byte
+
+const (
+	Horizontal Orientation = iota
+	Vertical
+)
+
+func Layout(boxes []Box, orientation Orientation) Box {
+	if orientation == Vertical {
+		content := ""
+		for _, b := range boxes {
+			content += b.String()
+		}
+		return NewBox(content)
+	}
+	if orientation == Horizontal {
+		w := 0
+		h := 0
+
+		for _, b := range boxes {
+			w += b.w
+			h = max(h, b.h)
+		}
+
+		content := ""
+		for r := 0; r < h; r++ {
+			line := ""
+			for _, b := range boxes {
+				if r+1 > b.h {
+					line += strings.Repeat(" ", b.w)
+					line += "|"
+					continue
+				}
+				s := b.content[r]
+				line += s[:len(s)-1]
+				line += "|"
+			}
+			line += "\n"
+			content += line
+		}
+
+		return NewBox(content)
+	}
+	return Box{}
 }
 
 func renderBoard(board board) string {
@@ -175,16 +252,11 @@ type uiInfo struct {
 	banner   string // элемент для вывода текстовой информации хода игры
 	mainview string // элемент для вывода игрового интерфейса
 	sideview string // элемент для вывода вспомогательной текстовой информации
-	// история ходов
-	err error // элемент для вывода ошибок пользовательского ввода
+	history  string // история ходов
+	err      error  // элемент для вывода ошибок пользовательского ввода
 }
 
 func main() {
-	// in := "abc\nxyzxyz\nsome\n"
-	// box := NewBox(in)
-	// fmt.Println(in)
-	// fmt.Println(box)
-
 	app := app{
 		board: board{
 			[]rune{'♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'},
